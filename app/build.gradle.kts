82d172a86b084d299d9710db82fe34da10d4cff9
cesar <cesarregalado@my.unt.edu>
2025-04-30 22:12:23 -0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation(libs.androidx.core.ktx)
     implementation("com.google.android.material:material:1.12.0")
 
     implementation(libs.androidx.activity.compose)
